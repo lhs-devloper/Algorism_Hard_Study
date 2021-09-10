9307ec1d2a629e0b696cfb7200bd252cd1e732d2
웰-노운 이현서 <sdsd0114@naver.com>
2021-09-11 06:32:14 +0900

    Update Algorism v2

---
+++ 정렬/MergeSort.py
@@ -0,0 +1,32 @@
+def merge_split(data):
+    if len(data) <= 1:
+        return data
+
+    medium = int(len(data) / 2)
+    left = merge_split(data[:medium])
+    right = merge_split(data[medium:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = list()
+    left_point, right_point = 0, 0
+
+    while len(left) > left_point and len(right) > right_point:
+        if left[left_point] > right[right_point]:
+            merged.append(right[right_point])
+            right_point += 1
+        else:
+            merged.append(left[left_point])
+            left_point += 1
+
+    while len(left) > left_point:
+        merged.append(left[left_point])
+        left_point += 1
+
+    while len(right) > right_point:
+        merged.append(right[right_point])
+        right_point += 1
+
+    return merged
---
+++ 정렬/QuickSort.py
@@ -0,0 +1,17 @@
+def quicksort(x):
+    if len(x) <= 1:
+        return x
+
+    pivot = x[len(x) // 2]
+    less = []
+    more = []
+    equal = []
+    for a in x:
+        if a < pivot:
+            less.append(a)
+        elif a > pivot:
+            more.append(a)
+        else:
+            equal.append(a)
+
+    return quicksort(less) + equal + quicksort(more)
